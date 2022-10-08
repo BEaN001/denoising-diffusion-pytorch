f15dbe0b0955f72667643c4cdd03dd33176eab42
BinYu <binyu17812@gmail.com>
2022-10-08 16:38:57 +0800

    annotation

---
+++ denoising_diffusion_pytorch/test_time_embedding.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch
+import math
+
+
+# sinusoidal positional embeds
+
+class SinusoidalPosEmb(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        device = x.device
+        half_dim = self.dim // 2
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
+        emb = x[:, None] * emb[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        return emb
+
+if __name__ == "__main__":
+    x = torch.randint(10, (2, 1))
+    print(x)
+    embedding_table = SinusoidalPosEmb(10)
+    print(embedding_table(x))
+
